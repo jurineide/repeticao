43c1a32ed55582054fa7907c44e83e2e1c69b718
jurineide <jurineidesouza@gmail.com>
2023-08-30 10:28:21 -0300

    for

---
+++ src/For.java
@@ -0,0 +1,9 @@
+public class For {
+
+    public static void main(String[] args) {
+        for(int carneiro =1; carneiro<=20;carneiro++){
+            System.out.println("contando carneiros"+ carneiro);
+        }
+        System.out.println(" ele dormiu");
+    }
+}
